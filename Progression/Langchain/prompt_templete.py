81b6d2848968d0556ba11092266c9b999db5514a
sudharshan-greytip <sudharshan@greytip.com>
2026-07-14 20:37:30 +0530

    Prompt templete with langchain

--- Progression/Langchain/prompt_templete.py
+++ Progression/Langchain/prompt_templete.py
@@ -5,12 +5,35 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
+name = input("Enter your name ")
+height = int(input("Enter your height "))
+weight = int(input("Enter your weight "))
 llm = ChatGoogleGenerativeAI(
-    model = "gemini-2.5-flash",
-    api_key = os.getenv("GEMINI_API_KEY"),
+    model='gemini-2.5-flash',
+    api_key= os.getenv("GEMINI_API_KEY"),
 )
-destination = "Bangalore"
-templete = PromptTemplate.from_template("List some of the temples in {destination}")
-prompt = templete.format(destination = destination)
+
+templete = PromptTemplate(
+    template = '''
+    Calculate the BMI for the provided inputs {name},{height},{weight} and explain what is BMI number means
+    ''',
+    input_variables = [
+        "name","height","weight"
+    ],
+)
+
+prompt = templete.format(name=name,height=height, weight=weight)
 response = llm.invoke(prompt)
 print(response.content)
+
+
+# llm = ChatGoogleGenerativeAI(
+#     model = "gemini-2.5-flash",
+#     api_key = os.getenv("GEMINI_API_KEY"),
+# )
+# destination = "Bangalore"
+# templete = PromptTemplate.from_template("List some of the temples in {destination}")
+# prompt = templete.format(destination = destination)
+# response = llm.invoke(prompt)
+# print(response.content)
+
